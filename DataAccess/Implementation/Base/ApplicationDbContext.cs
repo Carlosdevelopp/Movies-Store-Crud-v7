ac816fcfee1803d9fa6ff2bb7268678775b10697
Carlosdevelopp <carlosdevelopp@gmail.com>
2022-11-18 23:35:43 -0600

    DbContext

---
+++ DataAccess/Implementation/Base/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using DataAccess.Models.Tables;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Implementation.Base
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+        {
+        }
+
+        public virtual DbSet<Movies> Movies { get; set; }   
+        public virtual DbSet<Genres> Genres { get; set; }
+
+        public ApplicationDbContext(DbContextOptions options) : base(options) { }   
+
+    }
+}
---
+++ DataAccess/Models/Tables/Actors.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Models.Tables
+{
+    public class Actors
+    {
+        public int ActorId { get; set; }    
+        public string FullName { get; set; }
+    }
+}
